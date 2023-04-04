4fadb53d26d5599cad9a78a2347dda024eafc6fe
ashlyn046 <ashlyn04nicole@gmail.com>
2023-04-04 14:20:45 -0600

    Getting the login to work and change the page

--- login.js
+++ login.js
@@ -1,5 +1,13 @@
 function login() {
+    event.preventDefault();
     const nameEl = document.querySelector("#username");
+    const passwordEl = document.querySelector("#password");
     localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
+    localStorage.setItem("password", passwordEl.value);
+    window.location.href = "friends.html";
+  }
+  
+  window.onload = function() {
+    const loginButton = document.querySelector("#login");
+    loginButton.addEventListener("click", login);
   }
