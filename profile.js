55412489c0f47dad3ad28f83750cbadae607878e
ashlyn046 <ashlyn04nicole@gmail.com>
2023-04-04 18:10:14 -0600

    not quite working

--- profile.js
+++ profile.js
@@ -1,14 +1,29 @@
   function loadSongs() {
     alert("in load");
+
+    //making an array to hold our songs
     let songs = [];
-    const songsText = localStorage.getItem('songs');
+
+    //getting the text from local storage
+    const songsText = localStorage.getItem('profSongs');
+
+    //if there is text (there are profile songs), then making songs array parse songs text
     if (songsText) {
         alert("in songsText is not empty");
       songs = JSON.parse(songsText);
     }
   
+    //this is making the table body element equal to the table we already have in profile.html
     const tableBodyEl = document.querySelector('#songs');
+
+      // check if tableBodyEl exists
+  if (!tableBodyEl) {
+    console.error('Table body element not found.');
+    return;
+  }
+
   
+    //if there are somgs to add, here we add them
     if (songs.length) {
         alert("songs length was not empty");
       for (const [i, song] of songs.entries()) {
@@ -30,7 +45,7 @@
       }
     } else {
         alert("in else");
-      tableBodyEl.innerHTML = '<tr><td colSpan=4>BNo songs yet!</td></tr>';
+      tableBodyEl.innerHTML = '<tr><td colSpan=2>No songs yet!</td></tr>'; //this didn't post :(
     }
   }
   
--- share.js
+++ share.js
@@ -1,17 +1,25 @@
 function share() {
+    //this block works
     const singerEl = document.querySelector("#singer");
     const titleEl = document.querySelector("#title");
-    const songs = JSON.parse(localStorage.getItem("songs")) || [];
-  
-    songs.push({
+
+    //this makes profsongs equal to the old list of songs
+    const profSongs = JSON.parse(localStorage.getItem("profSongs")) || [];
+
+    //this pushed on the next song
+    profSongs.push({
       singer: singerEl.value,
       title: titleEl.value,
     });
   
-    localStorage.setItem("songs", JSON.stringify(songs));
+    localStorage.setItem("profSongs", JSON.stringify(profSongs));
     alert("Song shared successfully!");
     singerEl.value = "";
     titleEl.value = "";
+
+    profSongs.forEach(song => {
+        console.log(`Name: ${song.singer}, Title: ${song.title}`);
+      });
 
     window.location.href = "profile.html";
   }
