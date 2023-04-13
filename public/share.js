28ef8b76e29183d01b9d9d3c0b44dfd5e8901660
ashlyn046 <ashlyn04nicole@gmail.com>
2023-04-13 17:42:24 -0600

    changing share.js to have apis

--- public/share.js
+++ public/share.js
@@ -1,45 +1,57 @@
-function share() {
-    //this block works
-    const singerEl = document.querySelector("#singer");
-    const titleEl = document.querySelector("#title");
+async function share() {
+    //now doing the api stuff
+    const newSong = { singer: singerEl.value, title: titleEl.value, user: localStorage.getItem("userName") };
 
-    //this makes profsongs equal to the old list of songs
-    const profSongs = JSON.parse(localStorage.getItem("profSongs")) || [];
-    const friendsSongs = JSON.parse(localStorage.getItem("friendsSongs")) || [];
+    try {
 
-    //this pushed on the next song
-    profSongs.push({
-      singer: singerEl.value,
-      title: titleEl.value,
-    });
-
-    //this pushed on the next song
-    friendsSongs.push({
-      singer: singerEl.value,
-      title: titleEl.value,
-      user: localStorage.getItem("userName")
-    })
-  
-    //setting the songs in local storage
-    localStorage.setItem("profSongs", JSON.stringify(profSongs));
-    alert("Song shared successfully!");
-
-    localStorage.setItem("friendsSongs", JSON.stringify(friendsSongs));
-    singerEl.value = "";
-    titleEl.value = "";
-
-    //logging the namesand titles of each song
-    profSongs.forEach(song => {
-        console.log(`Name: ${song.singer}, Title: ${song.title}`);
+      //here, we set response to whatever we get back from this call, which will be songs with our new song added
+      const response = await fetch('/api/song', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newSong),
       });
 
-    friendsSongs.forEach(song => {
-      console.log(`User: ${"NA"}, Name: ${song.singer}, Title: ${song.title}`);
-    });
+      // Store what the service gave us in local storage
+      const songs = await response.json();
+      localStorage.setItem('profSongs', JSON.stringify(songs));
+    } catch {
+      // If there was an error then just track scores locally
+      this.updateSongsLocal(newSong);
+    }
 
-    //changes browser window to show profile.html
     window.location.href = "profile.html";
 
+  }
+
+  function updateSongsLocal(){
+     //this block works
+     const singerEl = document.querySelector("#singer");
+     const titleEl = document.querySelector("#title");
+ 
+     //this makes profsongs equal to the old list of songs
+     const profSongs = JSON.parse(localStorage.getItem("profSongs")) || [];
+     const friendsSongs = JSON.parse(localStorage.getItem("friendsSongs")) || [];
+ 
+     //this pushed on the next song
+     profSongs.push({
+       singer: singerEl.value,
+       title: titleEl.value,
+     });
+ 
+     //this pushed on the next song
+     friendsSongs.push({
+       singer: singerEl.value,
+       title: titleEl.value,
+       user: localStorage.getItem("userName")
+     })
+   
+     //setting the songs in local storage
+     localStorage.setItem("profSongs", JSON.stringify(profSongs));
+     alert("Song shared successfully!");
+ 
+     localStorage.setItem("friendsSongs", JSON.stringify(friendsSongs));
+     singerEl.value = "";
+     titleEl.value = "";
   }
 
   window.onload = function() {
