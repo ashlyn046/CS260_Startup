00521c4143c5f6074e394138462577e31accb553
ashlyn046 <ashlyn04nicole@gmail.com>
2023-04-13 17:49:45 -0600

    got share.js working for profile

--- public/share.js
+++ public/share.js
@@ -1,60 +1,62 @@
 async function share() {
+
+  //getting singer and title elements
+  const singerEl = document.querySelector("#singer");
+  const titleEl = document.querySelector("#title");
+
     //now doing the api stuff
-    const newSong = { singer: singerEl.value, title: titleEl.value, user: localStorage.getItem("userName") };
+  const newSong = {singer: singerEl.value, title: titleEl.value, user: localStorage.getItem("userName")};
+  try {
+    //here, we set response to whatever we get back from this call, which will be songs with our new song added
+    const response = await fetch('/api/song', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newSong),
+    });
 
-    try {
-
-      //here, we set response to whatever we get back from this call, which will be songs with our new song added
-      const response = await fetch('/api/song', {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(newSong),
-      });
-
-      // Store what the service gave us in local storage
-      const songs = await response.json();
-      localStorage.setItem('profSongs', JSON.stringify(songs));
-    } catch {
-      // If there was an error then just track scores locally
-      this.updateSongsLocal(newSong);
-    }
-
-    window.location.href = "profile.html";
-
+    // Store what the service gave us in local storage
+    const songs = await response.json();
+    localStorage.setItem('profSongs', JSON.stringify(songs));
+  } catch {
+    // If there was an error then just track scores locally
+    this.updateSongsLocal(newSong);
   }
 
-  function updateSongsLocal(){
+  window.location.href = "profile.html";
+
+}
+
+function updateSongsLocal(){
      //this block works
-     const singerEl = document.querySelector("#singer");
-     const titleEl = document.querySelector("#title");
+    const singerEl = document.querySelector("#singer");
+    const titleEl = document.querySelector("#title");
  
      //this makes profsongs equal to the old list of songs
-     const profSongs = JSON.parse(localStorage.getItem("profSongs")) || [];
-     const friendsSongs = JSON.parse(localStorage.getItem("friendsSongs")) || [];
+    const profSongs = JSON.parse(localStorage.getItem("profSongs")) || [];
+    const friendsSongs = JSON.parse(localStorage.getItem("friendsSongs")) || [];
  
      //this pushed on the next song
-     profSongs.push({
-       singer: singerEl.value,
-       title: titleEl.value,
-     });
+    profSongs.push({
+      singer: singerEl.value,
+      title: titleEl.value,
+    });
  
      //this pushed on the next song
-     friendsSongs.push({
-       singer: singerEl.value,
-       title: titleEl.value,
-       user: localStorage.getItem("userName")
-     })
+    friendsSongs.push({
+      singer: singerEl.value,
+      title: titleEl.value,
+      user: localStorage.getItem("userName")
+    })
    
      //setting the songs in local storage
-     localStorage.setItem("profSongs", JSON.stringify(profSongs));
-     alert("Song shared successfully!");
+    localStorage.setItem("profSongs", JSON.stringify(profSongs));
  
-     localStorage.setItem("friendsSongs", JSON.stringify(friendsSongs));
-     singerEl.value = "";
-     titleEl.value = "";
-  }
+    localStorage.setItem("friendsSongs", JSON.stringify(friendsSongs));
+    singerEl.value = "";
+    titleEl.value = "";
+}
 
-  window.onload = function() {
-    const shareButton = document.querySelector("#share");
-    shareButton.addEventListener("click", share);
-  }
+window.onload = function() {
+  const shareButton = document.querySelector("#share");
+  shareButton.addEventListener("click", share);
+}
