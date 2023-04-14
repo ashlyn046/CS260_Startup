5e8a342f2f1498496366d919637edea85541814f
ashlyn046 <ashlyn04nicole@gmail.com>
2023-04-13 18:04:53 -0600

    Added getting picture to share

--- public/share.js
+++ public/share.js
@@ -56,7 +56,26 @@ function updateSongsLocal(){
     titleEl.value = "";
 }
 
+
+function displayPicture() {
+  const random = Math.floor(Math.random() * 1000);
+  fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+    .then((response) => response.json())
+    .then((data) => {
+      const containerEl = document.querySelector('#picture');
+
+      const width = containerEl.offsetWidth;
+      const height = containerEl.offsetHeight;
+
+      const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+      const imgEl = document.createElement('img');
+      imgEl.setAttribute('src', imgUrl);
+      containerEl.appendChild(imgEl);
+    });
+}
+
 window.onload = function() {
+  displayPicture();
   const shareButton = document.querySelector("#share");
   shareButton.addEventListener("click", share);
 }
